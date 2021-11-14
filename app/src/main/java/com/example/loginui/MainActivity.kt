3848270c621cdd0e513079af57807433449072cf
arjunbhardwaj <bhardwajarjun199@gmail.com>
2021-11-14 21:49:52 +0530

    Quesrion 1

--- app/src/main/java/com/example/loginui/MainActivity.kt
+++ app/src/main/java/com/example/loginui/MainActivity.kt
@@ -7,12 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val button
-        val editemail
-        button.setOnClickListener{
-            if (editemail.textisnullorblank())
-
         }
     }
 }
